28f2ee42165e64e3eb8a8e1ba9f0d27a9f83d418
Ant Man <34754995+aaronamendez@users.noreply.github.com>
2022-03-02 16:01:54 -0500

    Added router structure.

--- knexfile.js
+++ knexfile.js
@@ -5,52 +5,52 @@
  */
 
 const sharedConfig = {
-  useNullAsDefault: true,
-  migrations: { directory: "./data/migrations" },
-  pool: {
-    afterCreate: (conn, done) => conn.run("PRAGMA foreign_keys = ON", done),
-  },
+	useNullAsDefault: true,
+	migrations: { directory: './data/migrations' },
+	pool: {
+		afterCreate: (conn, done) => conn.run('PRAGMA foreign_keys = ON', done),
+	},
 };
 
 module.exports = {
-  development: {
-    client: "sqlite3",
-    connection: {
-      filename: "./dev.sqlite3",
-    },
-    ...sharedConfig,
-    seeds: { directory: "./data/seeds" },
-  },
+	development: {
+		client: 'sqlite3',
+		connection: {
+			filename: './data/dev.db3',
+		},
+		...sharedConfig,
+		seeds: { directory: './data/seeds' },
+	},
 
-  staging: {
-    client: "postgresql",
-    connection: {
-      database: "my_db",
-      user: "username",
-      password: "password",
-    },
-    pool: {
-      min: 2,
-      max: 10,
-    },
-    migrations: {
-      tableName: "knex_migrations",
-    },
-  },
+	staging: {
+		client: 'postgresql',
+		connection: {
+			database: 'my_db',
+			user: 'username',
+			password: 'password',
+		},
+		pool: {
+			min: 2,
+			max: 10,
+		},
+		migrations: {
+			tableName: 'knex_migrations',
+		},
+	},
 
-  production: {
-    client: "postgresql",
-    connection: {
-      database: "my_db",
-      user: "username",
-      password: "password",
-    },
-    pool: {
-      min: 2,
-      max: 10,
-    },
-    migrations: {
-      tableName: "knex_migrations",
-    },
-  },
+	production: {
+		client: 'postgresql',
+		connection: {
+			database: 'my_db',
+			user: 'username',
+			password: 'password',
+		},
+		pool: {
+			min: 2,
+			max: 10,
+		},
+		migrations: {
+			tableName: 'knex_migrations',
+		},
+	},
 };
